0f71db04a1e64e32d6124118061b2592c4372807
budmc29 <chirica.mugurel@gmail.com>
2018-01-07 23:11:07 +0000

    Make initial test fail

--- test_script.bash
+++ test_script.bash
@@ -1,13 +1,7 @@
 #!/usr/bin/env bats
 
 @test "it works", {
-    run bash example.sh "test"
+    run bash test_file.sh "test"
     [ "$status" -eq 0 ]
     [ "$output" = "test" ]
-}
-
-@test "it fails", {
-    run bash example.sh "2 test"
-    [ "$status" -eq 1 ]
-    [ "$output" = "" ]
 }
